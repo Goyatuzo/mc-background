2971978ae77834e529102d2a543804e1903a4ce4
Goyatuzo <yotagur@gmail.com>
2023-03-25 13:23:55 -0500

    Better error handling

--- mcbackground/tasks/player_sync.py
+++ mcbackground/tasks/player_sync.py
@@ -1,24 +1,28 @@
 import asyncio
 from ..servernet import get_server_file
-from ..db import cursor, conn
+from ..db import conn
 from json import loads
 
 async def store_players_in_database():
-	# Periodically call the same method by looping indefinitely
-	while True:
-		print("Preparing to update users...")
+	cursor = conn.cursor()
+	try:
+		# Periodically call the same method by looping indefinitely
+		while True:
+			print("Preparing to update users...")
 
-		fpath = get_server_file("usercache.json")
+			fpath = get_server_file("usercache.json")
 
-		with open(fpath) as f:
-			users = loads(f.read())
-			
-			cleaned_users = [(user["uuid"], user["name"]) for user in users]
+			with open(fpath) as f:
+				users = loads(f.read())
+				
+				cleaned_users = [(user["uuid"], user["name"]) for user in users]
 
-			cursor.executemany("INSERT OR REPLACE INTO Players(uuid, name) VALUES (?, ?)", cleaned_users)
-			conn.commit()
+				cursor.executemany("INSERT OR REPLACE INTO Players(uuid, name) VALUES (?, ?)", cleaned_users)
+				conn.commit()
 
-			print(f"Updated {len(cleaned_users)} users")
+				print(f"Updated {len(cleaned_users)} users")
 
-		# Run this in 30 minutes time
-		await asyncio.sleep(10)
+			# Run this in 30 minutes time
+			await asyncio.sleep(10)
+	except Exception as e:
+		print(e)
